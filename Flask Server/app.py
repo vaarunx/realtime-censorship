24cb64734269360d342806609205739a42d01ccd
vaarunx <varunrishwandh2001@gmail.com>
2023-02-23 22:01:25 +0530

    pushing refs

--- Flask Server/app.py
+++ Flask Server/app.py
@@ -1,19 +1,33 @@
 from flask import Flask, request
 import json
+import re
+import joblib
 from flask_cors import CORS
+ 
 
 
 app = Flask(__name__)
 
 CORS(app)
 
-# <[^>]*>
 
 
 @app.route('/sendData', methods=['POST'])
 def my_api_function():
     my_array = json.loads(request.data)
-    print(my_array)
+    pattern = r"<[^>]*>"
+
+    finalText = [item for item in my_array if not re.search(pattern, item)]
+
+
+    classifier = joblib.load('../bert-small-512-sentiment.sav')
+    prediction = classifier.predict(finalText)
+
+    print(prediction)
+
+    # print("My Array " , my_array)
+
+
     
     return 'Array received'
 
