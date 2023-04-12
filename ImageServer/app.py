ce50cf58535c81535c3b0fec1ca6eafe6c2fbc8a
thuhinkhanna <qor6119@gmail.com>
2023-04-12 15:04:51 +0530

    Done

--- ImageServer/app.py
+++ ImageServer/app.py
@@ -1,34 +1,22 @@
-
-from flask import Flask, request
-from tensorflow.keras.models import load_model
 import cv2
-import numpy as np
-import os
 import requests
-from flask_cors import CORS
-
-import base64
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import opennsfw2 as n2
 import tensorflow as tf
-from model import OpenNsfwModel, InputType
-from image_utils import create_yahoo_image_loader
-from image_utils import create_tensorflow_image_loader
-
-IMAGE_LOADER_TENSORFLOW = "tensorflow"
-IMAGE_LOADER_YAHOO = "yahoo"
+from flask_cors import CORS
+from flask import Flask, request
 
 app = Flask(__name__)
 
 CORS(app)
 
-# Building Model
-                
-def preprocess(file):
-    response = requests.get(file, stream=True)
-    return response.content
-    
+model = n2.make_open_nsfw_model()
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h1>Hello, World!</h1>"
 
 @app.route("/imageclassify", methods=['POST'])
 def imageclassify():
@@ -37,32 +25,27 @@ def imageclassify():
     labels=[]
     for i in data:
         if 'http' in i:
-            model = OpenNsfwModel()
-            input_type = InputType['BASE64_JPEG']
-            model.build(weights_path='./open_nsfw-weights.npy', input_type=input_type)
-            fn_load_image = lambda filename: np.array([base64.urlsafe_b64encode(filename)])
             response = requests.get(i, stream=True)
-            preprocessed_image = response.content
-            #print('preprocessed image = '+str(preprocessed_image))
-            image = fn_load_image(preprocessed_image)
-            print(type(preprocessed_image))
-            print(type(image))
-            predictions = model.predict(image)
-            result = "\tSFW score:\t{}\n\tNSFW score:\t{}".format(*predictions[0])
-            #print('result = '+str(result))
-            result = result.split('\t')
-            nsfw_score = float(result[4])
-            sfw_score = float(result[2][:-1])
+            image_data = response.content
+
+            pil_image = Image.open(BytesIO(image_data))
+            image = n2.preprocess_image(pil_image, n2.Preprocessing.YAHOO)
+
+            inputs = np.expand_dims(image, axis=0)
+            predictions = model.predict(inputs)
+
+            sfw_score, nsfw_score = predictions[0]
+            # print(i, sfw_score, nsfw_score)
+
             if nsfw_score>sfw_score:
                 prediction = 'NSFW'
             else:
                 prediction = 'Safe'
             labels.append(prediction)
-            #print('prediction = '+prediction)
-            sess.close()
-            tf.compat.v1.reset_default_graph()
+
         else:
             labels.append('img not found')
+
     unsafe_links = []
     print('labels: '+str(labels)+' len labels: '+str(len(labels))+' len data: '+str(len(data)))
     for i in range(len(data)):
--- ImageServer/test2.py
+++ ImageServer/test2.py
@@ -1,12 +1,14 @@
 import numpy as np
 import opennsfw2 as n2
 from PIL import Image
+import requests
+from io import BytesIO
 
 # Load and preprocess image.
 img_url1 = 'https://i.imgur.com/6nZbEdk.jpg'
 response1 = requests.get(img_url1, stream=True)
 image_data1 = response1.content
-pil_image = Image.open(image_data1)
+pil_image = Image.open(BytesIO(image_data1))
 image = n2.preprocess_image(pil_image, n2.Preprocessing.YAHOO)
 # The preprocessed image is a NumPy array of shape (224, 224, 3).
 
@@ -24,3 +26,4 @@ predictions = model.predict(inputs)
 # The shape of predictions is (num_images, 2).
 # Each row gives [sfw_probability, nsfw_probability] of an input image, e.g.:
 sfw_probability, nsfw_probability = predictions[0]
+print(sfw_probability, nsfw_probability)
